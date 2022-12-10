800bd03111a197c7c91bf6496e99c83bebe476af
Anu S <96256005+Anus97@users.noreply.github.com>
2022-12-10 14:38:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import {useState} from 'react'
+function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+  return (
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
+    </div>
+    <div className="subHeading">
+      <br />
+      <h2>it's {Date()} </h2>
+    </div>
+    <div className="input">
+      <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+      <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+      {  toDos.map((value)=>{
+
+      return(<div className="todo">
+        <div className="left">
+          <input type="checkbox" name="" id="" />
+          <p>{value}</p>
+        </div>
+        <div className="right">
+          <i className="fas fa-times"></i>
+        </div>
+      </div>)
+     }) }
+    </div>
+  </div>
+  );
+}
+
+export default App;
